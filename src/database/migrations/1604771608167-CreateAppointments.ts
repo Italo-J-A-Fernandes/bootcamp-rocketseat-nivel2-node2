5139d1b40594c236e414d5c3dbb466643fcc43f5
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-07 14:55:04 -0300

    Configurando e gerando a primeira migration

---
+++ src/database/migrations/1604771608167-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1604771608167 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
