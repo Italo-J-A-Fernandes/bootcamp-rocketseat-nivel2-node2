13ec9282dab4621e6931592ab4cd800376b4e5e9
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-09 02:04:26 -0300

    User service, useding typeorm repository

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { getRepository } from 'typeorm';
+import Users from '../models/Users';
+
+interface RequestDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: RequestDTO): Promise<Users> {
+    const usersRepository = getRepository(Users);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Emasil address already used.');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
