48ef2cee86d49d43ccef837e2454d32c993f51ca
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-09 02:16:11 -0300

    Adapting appointservice to insert user

--- src/services/CreateAppointmentsService.ts
+++ src/services/CreateAppointmentsService.ts
@@ -4,12 +4,15 @@ import Appointment from '../models/Appointments';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppoimentService {
-  public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+  public async execute({
+    provider_id,
+    date,
+  }: RequestDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
@@ -22,7 +25,7 @@ class CreateAppoimentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
