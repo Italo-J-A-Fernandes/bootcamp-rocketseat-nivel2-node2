e4b4ddda0da25404908032f591ee43f4e38d174e
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-08 23:20:11 -0300

    Melhorando migrations e models

--- src/database/migrations/1604771608167-CreateAppointments.ts
+++ src/database/migrations/1604771608167-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1604771608167
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
--- src/database/migrations/1604881263564-CreateUsers.ts
+++ src/database/migrations/1604881263564-CreateUsers.ts
@@ -23,6 +23,10 @@ export default class CreateUsers1604881263564 implements MigrationInterface {
             isUnique: true,
           },
           {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
             name: 'created_at',
             type: 'timestamp',
             default: 'now()',
